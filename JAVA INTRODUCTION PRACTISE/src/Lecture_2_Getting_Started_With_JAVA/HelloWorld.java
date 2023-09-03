9a55b11f7449a4e52eec2d89a5ab38b200a512ea
Shashank <ishashankraghuvanshi@gmail.com>
2023-09-03 09:03:59 +0530

    first commit

---
+++ JAVA INTRODUCTION PRACTISE/src/Lecture_2_Getting_Started_With_JAVA/HelloWorld.java
@@ -0,0 +1,9 @@
+package Lecture_2_Getting_Started_With_JAVA;
+
+public class HelloWorld 
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World Shashank");
+    }
+}
