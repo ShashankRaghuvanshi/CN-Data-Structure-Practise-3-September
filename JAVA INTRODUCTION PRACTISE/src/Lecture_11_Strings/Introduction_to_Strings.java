cd3502eb2569e15a8a6633a5427a9e0f649dd63c
Shashank <ishashankraghuvanshi@gmail.com>
2023-09-14 08:01:46 +0530

    Initial Commit

---
+++ JAVA INTRODUCTION PRACTISE/src/Lecture_11_Strings/Introduction_to_Strings.java
@@ -0,0 +1,20 @@
+package Lecture_11_Strings;
+
+public class Introduction_to_Strings 
+{
+    public static void main(String args[])
+    {
+        char arr[] = {'C','o','d','i','n','g'};
+
+        String str = "Coding";
+        System.out.println(str.length());
+
+        System.out.println(str.charAt(2));
+
+        String str1 = ""; //Empty string
+        System.out.println(str1.length());
+
+        // System.out.println(str.charAt(-1));
+        // System.out.println(str.charAt(10));
+    }
+}
