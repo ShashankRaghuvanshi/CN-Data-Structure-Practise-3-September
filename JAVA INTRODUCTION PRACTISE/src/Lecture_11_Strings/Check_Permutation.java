38e8a17e4f42e015c13de98fb23b51e103fb7d73
Shashank <ishashankraghuvanshi@gmail.com>
2023-09-17 14:35:57 +0530

    Initial Commit

--- JAVA INTRODUCTION PRACTISE/src/Lecture_11_Strings/Check_Permutation.java
+++ JAVA INTRODUCTION PRACTISE/src/Lecture_11_Strings/Check_Permutation.java
@@ -31,12 +31,49 @@ public class Check_Permutation
         System.out.println("true");
     }
 
+    public static boolean isPermutation(String str1, String str2)
+    {
+        if(str1.length() != str2.length())
+        {
+            return false;
+        }
+
+        int frequency[] = new int[256];
+        for(int i=0; i<256; i++)
+        {
+            frequency[i] = 0;
+        }
+
+        for(int i=0; i<str1.length(); i++)
+        {
+            char ch = str1.charAt(i);
+            frequency[ch]++;
+        }
+
+        for(int i=0; i<str2.length(); i++)
+        {
+            char ch = str1.charAt(i);
+            frequency[ch]--;
+        }
+
+        for(int i=0; i<256; i++)
+        {
+            if(frequency[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         String str1 = sc.nextLine();
         String str2 = sc.nextLine();
         checkPermuatation(str1,str2);
+        boolean isPermutation = isPermutation(str1, str2);
+        System.out.println(isPermutation);
         sc.close();
     }
 }
