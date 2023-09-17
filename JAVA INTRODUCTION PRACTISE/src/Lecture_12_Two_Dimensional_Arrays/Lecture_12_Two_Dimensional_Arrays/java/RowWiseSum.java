578520c780d94f27a7fca96a622cce015fecd5b1
Shashank <ishashankraghuvanshi@gmail.com>
2023-09-17 18:35:29 +0530

    Initial Commit

---
+++ JAVA INTRODUCTION PRACTISE/src/Lecture_12_Two_Dimensional_Arrays/Lecture_12_Two_Dimensional_Arrays/java/RowWiseSum.java
@@ -0,0 +1,43 @@
+package Lecture_12_Two_Dimensional_Arrays.Lecture_12_Two_Dimensional_Arrays.java;
+
+import java.util.Scanner;
+
+public class RowWiseSum 
+{
+    public static int[][] takeInput()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of row and column");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int takeInput[][] = new int[n][m];
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                takeInput[i][j] = sc.nextInt();
+            }
+        }
+        return takeInput;
+    }
+    public static void sumRowWise(int arr[][])
+    {
+        System.out.println("My 2d array");
+        for(int i=0; i<arr.length; i++)
+        {
+            int sum = 0;
+            for(int j=0; j<arr[i].length; j++)
+            {
+                sum = sum + arr[i][j];
+            }
+            System.out.println("Row "+(i+1)+" "+ sum);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int arr[][] = takeInput();
+        sumRowWise(arr);
+    }
+}
